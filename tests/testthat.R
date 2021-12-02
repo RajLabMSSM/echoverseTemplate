7b326e8d28a0c03e79b65c78a223bf9967209f8e
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2021-12-02 23:35:34 +0000

    Fix CITATION. Automatically infer package name in testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,6 @@
-pkg <- read.dcf("DESCRIPTION", fields = "Package")[1]
+desc_path <- list.files("../","^DESCRIPTION$",
+                        full.names = TRUE, recursive = TRUE)[1]
+pkg <- read.dcf(desc_path, fields = "Package")[1]
 library(testthat)
 library(pkg, character.only = TRUE)
 
