42c74526c2ea65486c14e4e47bda7183d120266e
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-11-11 22:16:46 +0000

    Remove is_32bit [skip ci]

--- tests/testthat/test-source_all.R
+++ tests/testthat/test-source_all.R
@@ -8,39 +8,33 @@ test_that("source_all works", {
   ## because testthat uses its own environment during testing.
   ##
   ## See here for usage of devtools::code_coverage() in this context:
-  ## https://github.com/r-lib/covr/issues/487
+  ## https://github.com/r-lib/covr/issues/487 
+    manual <- FALSE 
   
-    if(!is_32bit()){
-        manual <- FALSE 
-      
-        ## Create new test env
-        env <- testthat::test_env()
-        ## This function might be helpful 
-        ## but there's zero documentation on how to use it.
-        # testthat::source_test_setup(path = "tests/testthat", env = env)
-        
-        #### Successful sourcing ####
-        testthat::source_test_helpers(env = env)
-        # setwd(here::here())
-        source_all(
-          path = if(manual) "./R" else "../../R" ,
-          env = env)  
-        globals <- ls(envir = env)
-        message("globals: ",paste(globals,collapse = ", "))
-        # testthat::expect_true(
-        #   all(c("messager","message_parallel","source_all") %in% globals)
-        # )
+    ## Create new test env
+    env <- testthat::test_env()
+    ## This function might be helpful 
+    ## but there's zero documentation on how to use it.
+    # testthat::source_test_setup(path = "tests/testthat", env = env)
+    
+    #### Successful sourcing ####
+    testthat::source_test_helpers(env = env)
+    # setwd(here::here())
+    source_all(
+      path = if(manual) "./R" else "../../R" ,
+      env = env)  
+    globals <- ls(envir = env)
+    message("globals: ",paste(globals,collapse = ", "))
+    # testthat::expect_true(
+    #   all(c("messager","message_parallel","source_all") %in% globals)
+    # )
 
-        #### Failed sourcing #### 
-        ## Create new test env
-        env <- testthat::test_env() 
-        rm(list = ls(envir = env))
-        source_all(path = "typoooo",
-                                       envir = env) 
-        globals <- ls(envir = env)  
-        testthat::expect_false("messager" %in% globals)
-        
-    } else {
-        testthat::expect_null(NULL)
-    } 
+    #### Failed sourcing #### 
+    ## Create new test env
+    env <- testthat::test_env() 
+    rm(list = ls(envir = env))
+    source_all(path = "typoooo",
+                                   envir = env) 
+    globals <- ls(envir = env)  
+    testthat::expect_false("messager" %in% globals) 
 })
